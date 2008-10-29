b5e28decfb3a6cda17c54543e421118be4f14ac9
Frank Lanitz <frlan@users.sourceforge.net>
2008-10-29 19:39:12 +0000

    GeanySendMail: Version bump and little update of documentation
    git-svn-id: https://geany-plugins.svn.sourceforge.net/svnroot/geany-plugins/trunk@263 e8383189-8249-0410-b506-acc975200cbd

--- geanysendmail/src/geanysendmail.c
+++ geanysendmail/src/geanysendmail.c
@@ -45,7 +45,7 @@ GeanyFunctions	*geany_functions;
 PLUGIN_VERSION_CHECK(104)
 
 PLUGIN_SET_INFO(_("GeanySendMail"), _("A little plugin to send the current \
-file as attachment by user's favorite mailer"), "0.4", "Frank Lanitz <frank@frank.uvena.de>")
+file as attachment by user's favorite mailer"), "0.5svn", "Frank Lanitz <frank@frank.uvena.de>")
 
 /* Keybinding(s) */
 enum
